71ea95c741924a5351161c038bdd0e33ab1784e2
far120 <tatafar2018@gmail.com>
2024-09-21 15:47:43 +0300

    v1

---
+++ authentication-react/src/App.js
@@ -0,0 +1,29 @@
+import { Link, Route, Routes } from 'react-router-dom';
+import Login from './pages/authentication/regester & login/login';
+import Signup from './pages/authentication/regester & login/signup';
+import Auth from './pages/authentication/userdata/auth';
+import ShowAuth from './pages/authentication/userdata/show';
+import Update from './pages/authentication/userdata/updates';
+import NavBar from './pages/authentication/navbar/nav';
+import Profile from './pages/authentication/userdata/profile';
+
+function App() {
+  return (
+    <>
+    <NavBar />
+     <Routes>
+      <Route  path="/login" element={<Login />} />
+      <Route  path="/signup" element={<Signup />} />
+      <Route  path="/auth" element={<Auth />} />
+      <Route  path="/auth/:id" element={<ShowAuth />} />
+      <Route path="/updates/:id" element={<Update />}/> 
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+   
+  
+     
+    </>
+  );
+}
+
+export default App;
